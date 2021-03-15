a3bde4ad1152f86ffdc65b0ea35f0e44c4055734
“Jay <tranquang2407@gmail.com>
2021-03-15 18:43:03 -0400

    commit natour code

---
+++ test.js
@@ -0,0 +1,7 @@
+app.listen(3000, () => {
+	console.log("Express server started at port 3000");
+});
+app.post(3000, (req, res) => {
+	console.log("Express server started at port 3000");
+});
+app.post("/", (req, res) => {});
